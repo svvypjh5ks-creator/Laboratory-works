1f5f751a08619feae8aaff9936acc08c41c5d9c5
Bekarys <asimzanulyb@gmail.com>
2026-05-01 14:06:10 +0600

    TSIS папкасы қосылды

---
+++ TSIS/TSIS1_PhoneBook/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host":     "localhost",
+    "port":     5432,
+    "database": "phonebook",
+    "user":     "postgres",
+    "password": "1234",   # <-- замени на свой пароль
+}
